9cc1b788eb8726b76c0ca425c2951ee472da8da1
Jannis R <mail@jannisr.de>
2017-04-29 18:01:42 +0200

    tests for positions :white_check_mark:

--- test.js
+++ test.js
@@ -1,9 +1,75 @@
 'use strict'
 
 const test = require('tape')
-const todo = require('.')
+const floor = require('floordate')
+const isRoughlyEqual = require('is-roughly-equal')
+
+const positions = require('./positions')
 
 
 
-test('todo', (t) => {
+const minute = 60 * 1000
+const hour = 60 * minute
+const when = new Date(+floor(new Date(), 'day') + 10 * hour)
+
+const isValidWhen = (_when) => isRoughlyEqual(10 * hour, +when, _when * 1000)
+
+
+
+test('positions', (t) => {
+	positions(when)
+	.then((trains) => {
+		for (let train of trains) {
+			t.equal(typeof train.name, 'string')
+			t.ok(train.name)
+
+			if (typeof train.id !== 'string') {
+				console.log(train.id)
+				break
+			}
+			t.equal(typeof train.id, 'string')
+			t.ok(train.id)
+
+			if (train.previousStation) {
+				const p = train.previousStation
+				t.equal(p.type, 'station')
+
+				t.equal(typeof p.id, 'string')
+				t.ok(p.id)
+
+				t.equal(typeof p.name, 'string')
+				t.ok(p.name)
+
+				if (p.departure) {
+					t.equal(typeof p.departure, 'number')
+					t.ok(isValidWhen(p.departure))
+				}
+			}
+
+			if (train.nextStation) {
+				const n = train.previousStation
+				t.equal(n.type, 'station')
+
+				t.equal(typeof n.id, 'string')
+				t.ok(n.id)
+
+				t.equal(typeof n.name, 'string')
+				t.ok(n.name)
+
+				if (n.arrival) {
+					t.equal(typeof n.arrival, 'number')
+					t.ok(isValidWhen(n.arrival))
+				}
+			}
+
+			t.equal(typeof train.delay, 'number')
+			t.ok(train.delay >= 0)
+
+			t.equal(train.direction.type, 'station')
+			t.equal(typeof train.direction.name, 'string')
+			t.ok(train.direction.name)
+		}
+		t.end()
+	})
+	.catch(t.ifError)
 })
