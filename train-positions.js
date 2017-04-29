40b588a1ab3c0302ad7a10496527ea432b9c328d
Jannis R <mail@jannisr.de>
2017-04-29 11:40:04 +0200

    parse train positions

--- train-positions.js
+++ train-positions.js
@@ -3,8 +3,47 @@
 const moment = require('moment-timezone')
 const qs = require('querystring')
 const {fetch} = require('fetch-ponyfill')()
-const {parse} = require('content-type')
+const ct = require('content-type')
 const {decode} = require('iconv-lite')
+
+const parseTime = (time) => {
+	const m = moment().tz('Europe/Berlin')
+	m.hour(parseInt(time.slice(0, 2)))
+	m.minute(parseInt(time.slice(3, 5)))
+	return Math.round(m / 1000)
+}
+
+const parse = ([coords]) => {
+	const trains = []
+	for (let c of coords) {
+		trains.push({
+			name: c[0],
+			id: c[3],
+			previousStation: {
+				type: 'station',
+				id: c[10],
+				name: c[9],
+				departure: c[17] ? parseTime(c[17]) : null
+			},
+			nextStation: {
+				type: 'station',
+				id: c[12],
+				name: c[11],
+				arrival: c[16] ? parseTime(c[16]) : null,
+				delay: parseInt(c[18]) * 60 // in seconds
+			},
+			delay: parseInt(6) * 60, // in seconds
+			direction: {
+				type: 'station',
+				// todo: id
+				name: c[7]
+			}
+		})
+	}
+	// const c = coords.find((c) => c[0].indexOf('278') >= 0 && c[0].indexOf('ICE') >= 0)
+
+	return trains
+}
 
 const positions = (when = Date.now()) => {
 	const date = moment(when).tz('Europe/Berlin').format('YYYYMMDD')
@@ -31,7 +70,7 @@ const positions = (when = Date.now()) => {
 		if (!res.ok) {
 			throw new Error('response not ok: ' + res.status)
 		}
-		const {type} = parse(res.headers.get('content-type'))
+		const {type} = ct.parse(res.headers.get('content-type'))
 		if (type !== 'application/json') {
 			throw new Error('invalid response type: ' + type)
 		}
@@ -39,6 +78,7 @@ const positions = (when = Date.now()) => {
 		return res.buffer()
 	})
 	.then((raw) => JSON.parse(decode(raw, 'ISO-8859-1')))
+	.then(parse)
 }
 
 module.exports = positions
